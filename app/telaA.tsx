6e5a3aaa2dfa510788f664415f27b016b7b0a1cf
MateusDalfovo <125157446+MateusDalfovo@users.noreply.github.com>
2023-05-26 12:32:07 -0300

    Add files via upload

---
+++ app/telaA.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Tela A</Text>
+      <Link href="/telaB">Ir para tela B</Link>
+      <Text>{"\n"}</Text>
+      <Link href="/">Voltar para o Menu</Link>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+text:{
+    fontSize: 22,
+    fontWeight: 'bold',
+},
+});
